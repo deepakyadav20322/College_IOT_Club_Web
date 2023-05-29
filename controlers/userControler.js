6b14727ee84de6cb91590b34fea85dd4f7880daa
deepak <informationtechnology20322@@gmail.com>
2023-05-29 09:06:10 +0530

    third

--- controlers/userControler.js
+++ controlers/userControler.js
@@ -9,11 +9,11 @@ const { render } = require("../Routes/userRoute");
 //  Send the Varification email by multer.........................
 const sendverifyMail = async (fname, lastname, email, user_id) => {
   try {
-    const verificationLink = `https://college-iot-club-web.onrender.com/mailVerifyPage?id=${user_id}`;
-    // const verificationLink = `http://127.0.0.1:${process.env.PORT}/mailVerifyPage?id=${user_id}`;
+    var verificationLink = `https://college-iot-club-web.onrender.com/mailVerifyPage?id=${user_id}`;
+    // var verificationLink = `http://127.0.0.1:${process.env.PORT}/mailVerifyPage?id=${user_id}`;
     const transporter = nodemailer.createTransport({
       host: "smtp.gmail.com",
-      secure: false,
+     
       requireTls: true,
       auth: {
         user: process.env.emailUser,
@@ -515,6 +515,7 @@ module.exports = {
   forgetPassVarify,
   forgetPasswordLoad,
   forgetPassword,
+  
   directEmailVerifyLoad,
   directEmailVerify,
   logout,
